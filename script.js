d2e6b0b097081c2cd91fd8d79ca95395614f9e92
Ashot <yeghiazaryanashot94@gmail.com>
2023-05-05 17:33:28 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let mainBox = document.querySelector(".mainBox");
+
+let monitor = document.querySelector('.mainBoxSection1');
+
+mainBox.addEventListener('click',(event)=>{    
+    if(!event.target.classList.contains('but'))return
+    let action = event.target.innerText
+  
+    switch(action){
+        case 'AC':monitor.innerHTML = ''
+        break;
+        case '=':monitor.innerHTML = eval(monitor.innerHTML).toFixed(2)
+        break;
+        case 'Del':monitor.innerHTML = monitor.innerHTML.slice(0, -1)
+        break;       
+        case '00':{
+            if(monitor.innerHTML != '')return monitor.innerHTML += action
+                return monitor.innerHTML += ''
+        } 
+        // break;
+        // case '0':{
+        //     if(monitor.innerHTML != '')return monitor.innerHTML += action
+        //         return monitor.innerHTML += ''
+        // } 
+        // break;
+        case '+':{
+            if(monitor.innerHTML != '')return monitor.innerHTML += action
+                return monitor.innerHTML += ''
+        } 
+        // break;
+        case '-':{
+            if(monitor.innerHTML != '')return monitor.innerHTML += action
+                return monitor.innerHTML += ''
+        } 
+        // break;
+        case '*':{
+            if(monitor.innerHTML != '')return monitor.innerHTML += action
+                return monitor.innerHTML += ''
+        } 
+        // break;
+        case '/':{
+            if(monitor.innerHTML != '')return monitor.innerHTML += action
+                return monitor.innerHTML += ''
+        } 
+        // break;
+        case '.':{
+            if(monitor.innerHTML != '')return monitor.innerHTML += action
+                return monitor.innerHTML += ''
+        } 
+        // break;
+        
+
+        default:monitor.innerHTML += action 
+    }
+    
+})
+
+
+
